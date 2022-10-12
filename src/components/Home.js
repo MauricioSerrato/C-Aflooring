6141104d15c17ea6fd7d843cdebf99b8f0d70102
MauricioSerrato <mserato99@gmail.com>
2022-10-12 15:13:21 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+
+
+
+const Home = () => {
+    return(
+        <>
+        <div className ='homepage'> 
+        <div className='slogan'>
+            <h3>Its more than Flooring its <h3 className="blue">Craftsmanship</h3> </h3>
+        </div>
+        
+        </div>
+
+        <div className='homeextra'>
+            <div> <h2 className="blue">15</h2> <br/>
+            <p >Years in Business</p>
+            </div>
+          
+           <div className="vl"></div>
+           <div> <h2 className="blue">5000+</h2> <br/> <p> Projects Completed</p></div>
+       
+           <div className="vl"></div>
+           <div><h2 className="blue">1M+</h2> <br/> <p> SQFT Built</p></div>
+           
+        </div>
+        </>
+    )
+}
+
+
+export default Home 
